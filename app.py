fb79fd13545249146aa1f81493076dca6c2e7f80
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2023-04-23 12:22:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,6 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 tsla_data = yf.download("TSLA", start="2015-01-01")
 
 def visualize_stock_price_history():
-    plt.figure(figsize=(16,8))
-    plt.plot(tsla_data.index, tsla_data['Close'])
-    plt.xlabel('Date')
-    plt.ylabel('Closing price ($)')
-    plt.title('Tesla (TSLA) Stock Price History')
-    st.pyplot()
-
-def visualize_technical_indicators():
     # Relative Strength Index (RSI)
     delta = tsla_data['Close'].diff()
     gain = delta.where(delta > 0, 0)
@@ -33,15 +25,18 @@ def visualize_technical_indicators():
     high_14, low_14 = tsla_data['High'].rolling(window=14).max(), tsla_data['Low'].rolling(window=14).min()
     k_percent = 100 * ((tsla_data['Close'] - low_14) / (high_14 - low_14))
     d_percent = k_percent.rolling(window=3).mean()
-    
-    # Plot RSI and Stochastic Oscillator
-    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(16,8))
-    axes[0].plot(tsla_data.index, rsi)
-    axes[0].set_title('Relative Strength Index (RSI)')
-    axes[1].plot(tsla_data.index, k_percent, label='%K')
-    axes[1].plot(tsla_data.index, d_percent, label='%D')
-    axes[1].set_title('Stochastic Oscillator')
-    axes[1].legend()
+
+    # Plot stock price history, RSI, and Stochastic Oscillator
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(16,12), sharex=True)
+    axes[0].plot(tsla_data.index, tsla_data['Close'])
+    axes[0].set_title('Tesla (TSLA) Stock Price History')
+    axes[0].set_ylabel('Closing price ($)')
+    axes[1].plot(tsla_data.index, rsi)
+    axes[1].set_title('Relative Strength Index (RSI)')
+    axes[2].plot(tsla_data.index, k_percent, label='%K')
+    axes[2].plot(tsla_data.index, d_percent, label='%D')
+    axes[2].set_title('Stochastic Oscillator')
+    axes[2].legend()
     st.pyplot()
 
 def predict_stock_price(p, d, q):
@@ -66,13 +61,10 @@ def main():
     st.title("Tesla (TSLA) Stock Price Analysis")
 
     # Sidebar options
-    option = st.sidebar.selectbox("Select an option", ("Stock Price History", "Technical Indicators", "Predict Future Prices"))
+    option = st.sidebar.selectbox("Select an option", ("Stock Price History and Technical Indicators", "Predict Future Prices"))
 
-    if option == "Stock Price History":
+    if option == "Stock Price History and Technical Indicators":
         visualize_stock_price_history()
-
-    elif option == "Technical Indicators":
-        visualize_technical_indicators()
 
     else:
         st.write("Enter ARIMA model parameters:")
